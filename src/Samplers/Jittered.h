0c2847c1851c200a05e41a400a16ce06e55776a9
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-24 18:39:15 -0300

    added initial support for tests (gtest), added samplers (missing rand functions)

---
+++ src/Samplers/Jittered.h
@@ -0,0 +1,25 @@
+#ifndef __JITTERED__
+#define __JITTERED__
+
+#include "Sampler.h"
+#include <Eigen/Dense>
+using Eigen::Vector2d;
+
+class Jittered: public Sampler {
+  
+ public:		
+  Jittered();								
+  Jittered(const int num_samples);					
+  Jittered(const int num_samples, const int m);	
+  Jittered(const Jittered& u);					
+  Jittered& operator= (const Jittered& rhs);				
+  virtual Jittered* clone() const;			
+  virtual ~Jittered();
+		
+ private:
+  virtual void generate_samples();
+  
+};
+
+#endif
+
