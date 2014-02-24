2828456b6d9fe7414c3a906142d8321111495a7f
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-24 18:51:00 -0300

    added PerfectSpecular

---
+++ src/BRDFs/PerfectSpecular.cpp
@@ -0,0 +1,46 @@
+#include "PerfectSpecular.h"
+
+
+PerfectSpecular::PerfectSpecular() :
+  BRDF(),
+  kr(0.0), 
+  cr(1.0)
+{}
+
+
+PerfectSpecular::~PerfectSpecular() {}
+
+
+PerfectSpecular* PerfectSpecular::clone() const {
+  return new PerfectSpecular(*this);
+}	
+
+
+RGBColor PerfectSpecular::f(const ShadeRec& sr, const Vector3d& wo, const Vector3d& wi) const {
+  return black;
+}
+
+
+/* this computes wi: the direction of perfect mirror reflection
+   it's called from from the functions Reflective::shade and Transparent::shade.
+   the fabs in the last statement is for transparency */
+RGBColor PerfectSpecular::sample_f(const ShadeRec& sr, const Vector3d& wo, Vector3d& wi) const {
+  float ndotwo = sr.normal.dot(wo);
+  wi = -wo + 2.0 * sr.normal * ndotwo; 
+  return kr * cr / fabs(sr.normal.dot(wi)); // why is this fabs? // kr would be a Fresnel term in a Fresnel reflector for transparency when ray hits inside surface?, if so it should go in Chapter 24
+}
+
+
+/* this version of sample_f is used with path tracing
+   it returns ndotwi in the pdf */
+RGBColor PerfectSpecular::sample_f(const ShadeRec& sr, const Vector3d& wo, Vector3d& wi, float& pdf) const {
+  float ndotwo = sr.normal.dot(wo);
+  wi = -wo + 2.0 * sr.normal * ndotwo; 
+  pdf = fabs(sr.normal.dot(wi));
+  return kr * cr;  
+}
+
+
+RGBColor PerfectSpecular::rho(const ShadeRec& sr, const Vector3d& wo) const {
+  return black;
+}
