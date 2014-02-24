ae6294ff34784ae98c47174b7ed37b2d6acfb24e
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-23 23:35:18 -0300

    directional ok

--- src/Lights/Directional.cpp
+++ src/Lights/Directional.cpp
@@ -1,69 +1,48 @@
 #include "Directional.h"
 
-// ---------------------------------------------------------------------- default constructor
-
-Directional::Directional(void)
-	: 	Light(),
-		ls(1.0),
-		color(1.0),
-		dir(0, 1, 0)			
+Directional::Directional() :
+  Light(),
+  ls(1.0),
+  color(1.0),
+  dir(0, 1, 0)			
 {}
 
 
-// ---------------------------------------------------------------------- dopy constructor
-
-Directional::Directional(const Directional& dl)
-	: 	Light(dl),
-		ls(dl.ls),
-		color(dl.color),
-		dir(dl.dir)  		
+Directional::Directional(const Directional& dl) :
+  Light(dl),
+  ls(dl.ls),
+  color(dl.color),
+  dir(dl.dir)  		
 {}
 
 
-// ---------------------------------------------------------------------- clone
-
-Light* 
-Directional::clone(void) const {
-	return (new Directional(*this));
+Light* Directional::clone() const {
+  return new Directional(*this);
 }
 
 
-// ---------------------------------------------------------------------- assignment operator
+Directional& Directional::operator= (const Directional& rhs) {
+  if (this != &rhs) {
+    Light::operator= (rhs);
 
-Directional& 
-Directional::operator= (const Directional& rhs) 	
-{
-	if (this == &rhs)
-		return (*this);
-			
-	Light::operator= (rhs);
-	
-	ls		= rhs.ls;
-	color 	= rhs.color;
-	dir 	= rhs.dir;
-
-	return (*this);
+    ls	  = rhs.ls;
+    color = rhs.color;
+    dir   = rhs.dir;
+  }
+  
+  return *this;
 }
 
 
-// ---------------------------------------------------------------------- destructor																			
-
-Directional::~Directional(void) {}
+Directional::~Directional() {}
 
 
-// ---------------------------------------------------------------------- get_direction
-// as this function is virtual, it shouldn't be inlined 
-
-Vector3D								
-Directional::get_direction(ShadeRec& sr) {
-	return (dir);
+/* as this function is virtual, it shouldn't be inlined */
+Vector3d Directional::get_direction(ShadeRec& sr) {
+  return dir;
 }	
 
-// ------------------------------------------------------------------------------  L
 
-RGBColor
-Directional::L(ShadeRec& s) {	
-	return (ls * color);
+RGBColor Directional::L(ShadeRec& s) {	
+  return ls * color;
 }
-
-
--- src/Lights/Directional.h
+++ src/Lights/Directional.h
@@ -2,110 +2,72 @@
 #define __DIRECTIONAL__
 
 #include "Light.h"
-#include "Vector3D.h"
 #include "RGBColor.h"
-
-#include "World.h"			// you will need this later on for shadows
+#include "World.h"          // you will need this later on for shadows
 #include "ShadeRec.h"
-
+#include <Eigen/Dense>
+using Eigen::Vector3d;
 
 class Directional: public Light {
-	public:
-	
-		Directional(void);   							
-
-		Directional(const Directional& dl); 
+  
+ public:
+  Directional();   							
+  Directional(const Directional& dl); 
+  virtual Light* clone() const;			
+  Directional& operator= (const Directional& rhs); 
+  virtual ~Directional(); 				
+  void scale_radiance(const float b);
+  void set_color(const float c);
+  void set_color(const RGBColor& c);
+  void set_color(const float r, const float g, const float b); 		
+  void set_direction(Vector3d d);
+  void set_direction(float dx, float dy, float dz);
+  virtual Vector3d get_direction(ShadeRec& sr);
+  virtual RGBColor L(ShadeRec& sr);	
 		
-		virtual Light* 									
-		clone(void) const;			
-
-		Directional& 									
-		operator= (const Directional& rhs); 
-			
-		virtual											
-		~Directional(void); 
-				
-		void
-		scale_radiance(const float b);
-		
-		void
-		set_color(const float c);
-		
-		void
-		set_color(const RGBColor& c);
-		
-		void
-		set_color(const float r, const float g, const float b); 		
-			
-		void
-		set_direction(Vector3D d);						
-		
-		void
-		set_direction(float dx, float dy, float dz);
-		
-		virtual Vector3D								
-		get_direction(ShadeRec& sr);
-				
-		virtual RGBColor		
-		L(ShadeRec& sr);	
-		
-	private:
-
-		float		ls;			
-		RGBColor	color;
-		Vector3D	dir;		// direction the light comes from
+ private:
+  float	   ls;			
+  RGBColor color;
+  Vector3d dir;                 // direction the light comes from
+  
 };
 
 
-// inlined access functions
-
-
-// ------------------------------------------------------------------------------- scale_radiance
-
-inline void
-Directional::scale_radiance(const float b) { 
-	ls = b;
-}
-
-// ------------------------------------------------------------------------------- set_color
-
-inline void
-Directional::set_color(const float c) {
-	color.r = c; color.g = c; color.b = c;
+inline void Directional::scale_radiance(const float b) { 
+  ls = b;
 }
 
 
-// ------------------------------------------------------------------------------- set_color
-
-inline void
-Directional::set_color(const RGBColor& c) {
-	color = c;
+inline void Directional::set_color(const float c) {
+  color.r = c;
+  color.g = c;
+  color.b = c;
 }
 
 
-// ------------------------------------------------------------------------------- set_color
-
-inline void
-Directional::set_color(const float r, const float g, const float b) {
-	color.r = r; color.g = g; color.b = b;
+inline void Directional::set_color(const RGBColor& c) {
+  color = c;
 }
 
 
-// ---------------------------------------------------------------------- set_direction
-
-inline void
-Directional::set_direction(Vector3D d) {
-	dir = d;
-	dir.normalize();
+inline void Directional::set_color(const float r, const float g, const float b) {
+  color.r = r;
+  color.g = g;
+  color.b = b;
 }
 
 
-// ---------------------------------------------------------------------- set_direction 
+inline void Directional::set_direction(Vector3d d) {
+  dir = d;
+  dir.normalize();
+}
 
-inline void
-Directional::set_direction(float dx, float dy, float dz) {
-	dir.x = dx; dir.y = dy; dir.z = dz;
-	dir.normalize();
+
+inline void Directional::set_direction(float dx, float dy, float dz) {
+  dir(0) = dx;
+  dir(1) = dy;
+  dir(2) = dz;  
+  dir.normalize();
 }
 
 
