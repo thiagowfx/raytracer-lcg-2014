3b78a91657560306091d759abab46b3fc084ba09
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-23 22:29:03 -0300

    Broken code now, moving to Eigen3. Removed legacy geometry. Ray OK

--- src/Utilities/Ray.cpp
+++ src/Utilities/Ray.cpp
@@ -1,43 +1,31 @@
 #include "Ray.h"
 
-// ---------------------------------------------------------------- default constructor
-
-Ray::Ray (void)
-	: 	o(0.0), 
-		d(0.0, 0.0, 1.0) 
+Ray::Ray () :
+  o(Vector3d::Zero()),
+  d(0.0, 0.0, 1.0) 
 {}
 
-// ---------------------------------------------------------------- constructor
 
-Ray::Ray (const Point3D& origin, const Vector3D& dir)
-	: 	o(origin), 
-		d(dir) 
+Ray::~Ray () {}
+
+
+Ray::Ray (const Vector3d& origin, const Vector3d& dir) :
+  o(origin), 
+  d(dir) 
 {}
 
-// ---------------------------------------------------------------- copy constructor
 
-Ray::Ray (const Ray& ray)
-	: 	o(ray.o), 
-		d(ray.d) 
+Ray::Ray (const Ray& ray) :
+  o(ray.o), 
+  d(ray.d) 
 {}
 
-// ---------------------------------------------------------------- assignment operator
 
-Ray& 
-Ray::operator= (const Ray& rhs) {
-	
-	if (this == &rhs)
-		return (*this);
-		
-	o = rhs.o; 
-	d = rhs.d; 
+Ray& Ray::operator= (const Ray& rhs) {
+  if (this != &rhs) {
+    o = rhs.o; 
+    d = rhs.d;
+  }
 
-	return (*this);	
+  return *this;
 }
-
-// ---------------------------------------------------------------- destructor
-
-Ray::~Ray (void) {}
-
-
-
--- src/Utilities/Ray.h
+++ src/Utilities/Ray.h
@@ -1,25 +1,20 @@
 #ifndef __RAY__
 #define __RAY__
 
-#include "Point3D.h"
-#include "Vector3D.h"
+#include <Eigen/Dense>
+using Eigen::Vector3d;
 
 class Ray {
-	public:
-	
-		Point3D			o;  	// origin 
-		Vector3D		d; 		// direction 
-		
-		Ray(void);			
-		
-		Ray(const Point3D& origin, const Vector3D& dir);	
-		
-		Ray(const Ray& ray); 		
-		
-		Ray& 						
-		operator= (const Ray& rhs);
-		 								
-		~Ray(void);
+  
+ public:
+  Vector3d o;                   // origin (point)
+  Vector3d d;                   // direction (vector)
+  Ray();
+  ~Ray();
+  Ray(const Vector3d& origin, const Vector3d& dir);	
+  Ray(const Ray& ray); 		
+  Ray& operator= (const Ray& rhs); 
+  
 };
 
 #endif
