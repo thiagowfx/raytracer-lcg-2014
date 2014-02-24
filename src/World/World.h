d0fc5a198c30da99875b0c76d82c68fdb28f4b54
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-24 09:49:13 -0300

    refactored world.h

--- src/World/World.h
+++ src/World/World.h
@@ -10,7 +10,6 @@
    the Tracer base class contains a pointer to the world. If we wrote a correct copy constructor for the 
    Tracer class, the World copy construtor would call itself recursively until we ran out of memory. */
 
-
 #include "Ambient.h"
 #include "Camera.h"
 #include "Constants.h"
@@ -27,102 +26,59 @@
 #include <cstring>              // for png++ streerror
 #include <png++/png.hpp>
 #include <vector>
-class GeometricObject;
-
+#include "GeometricObject.h"
 using namespace std;
 
-
-class World {	
-	public:
-	
-		ViewPlane					vp;
-		RGBColor					background_color;
-		Tracer*						tracer_ptr;
-		Light*   					ambient_ptr;
-		Camera*						camera_ptr;		
-		vector<GeometricObject*>	objects;		
-		vector<Light*> 				lights;
+class World {
+ 
+ public:	
+  ViewPlane		   vp;
+  RGBColor		   background_color;
+  Tracer*		   tracer_ptr;
+  Light*   		   ambient_ptr;
+  Camera*		   camera_ptr;		
+  vector<GeometricObject*> objects;		
+  vector<Light*> 	   lights;
 		
-			
-
-	public:
-	
-		World(void);												
-		
-		~World();
-								
-		void 
-		add_object(GeometricObject* object_ptr);
-		
-		void 
-		add_light(Light* light_ptr); 
-		
-		void
-		set_ambient_light(Light* light_ptr);			
-		
-		void
-		set_camera(Camera* c_ptr);	 
-
-		void 					
-		build(void);
-
-		void 												
-		render_scene(FILE *fp) const;
+  World();												
+  ~World();
+  void add_object(GeometricObject* object_ptr);
+  void add_light(Light* light_ptr); 
+  void set_ambient_light(Light* light_ptr);			
+  void set_camera(Camera* c_ptr);	 
+  void build();
+  void render_scene(FILE *fp) const;
+  RGBColor max_to_one(const RGBColor& c) const;
+  RGBColor clamp_to_color(const RGBColor& c) const;
+  void display_pixel(const int row, const int column, const RGBColor& pixel_color, FILE *fp) const;
+  ShadeRec hit_objects(const Ray& ray);
+  void file_to_png(FILE *fp, const char *imageFile);
 						
-		RGBColor
-		max_to_one(const RGBColor& c) const;
-		
-		RGBColor
-		clamp_to_color(const RGBColor& c) const;
-		
-		void
-                  display_pixel(const int row, const int column, const RGBColor& pixel_color, FILE *fp) const;
-
-		ShadeRec
-		hit_objects(const Ray& ray);
-
-                void file_to_png(FILE *fp, const char *imageFile);
-		
-						
-	private:
-		
-		void 
-		delete_objects(void);
-		
-		void 
-		delete_lights(void);
+ private:
+  void delete_objects();
+  void delete_lights();
+  
 };
 
 
-// ------------------------------------------------------------------ add_object
-
-inline void 
-World::add_object(GeometricObject* object_ptr) {  
-	objects.push_back(object_ptr);	
+inline void World::add_object(GeometricObject* object_ptr) {  
+  objects.push_back(object_ptr);	
 }
 
 
-// ------------------------------------------------------------------ add_light
-
-inline void 
-World::add_light(Light* light_ptr) {  
-	lights.push_back(light_ptr);
+inline void World::add_light(Light* light_ptr) {  
+  lights.push_back(light_ptr);
 }
 
 
-// ------------------------------------------------------------------ set_ambient_light
-
-inline void
-World::set_ambient_light(Light* light_ptr) {
-	ambient_ptr = light_ptr;
+inline void World::set_ambient_light(Light* light_ptr) {
+  ambient_ptr = light_ptr;
 }
 
 
-// ------------------------------------------------------------------ set_camera
-
-inline void
-World::set_camera(Camera* c_ptr) {
-	camera_ptr = c_ptr;
+inline void World::set_camera(Camera* c_ptr) {
+  camera_ptr = c_ptr;
 }
+
 
 #endif
