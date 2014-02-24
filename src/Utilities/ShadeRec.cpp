a6acda38b9f17080b7551758e8fc7c6b9df33708
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-23 22:59:58 -0300

    ShadeRec ok

--- src/Utilities/ShadeRec.cpp
+++ src/Utilities/ShadeRec.cpp
@@ -1,46 +1,35 @@
-// this file contains the definition of the class ShadeRec
+/* there is no default constructor as the World reference always has to be initialised
+   there is also no assignment operator as we don't want to assign the world
+   the copy constructor only copies the world reference
+   the ray tracer is written so that new ShadeRec objects are always constructed
+   using the first constructor or the copy constructor */
 
-// there is no default constructor as the World reference has to be initialised
-// there is also no assignment operator as we don't want to assign the world
-// the copy constructor only copies the world reference
-// the ray tracer is written so that new ShadeRec objects are always constructed
-// using the first constructor or the copy constructor
-
-#include "Constants.h"
 #include "ShadeRec.h"
 
-// ------------------------------------------------------------------ constructor
-
-ShadeRec::ShadeRec(World& wr)
-	: 	hit_an_object(false),
-		material_ptr(NULL),
-		hit_point(),
-		local_hit_point(),
-		normal(),
-		ray(),
-		depth(0),
-		t(0.0),
-		w(wr)
+ShadeRec::ShadeRec(World& wr) :
+  hit_an_object(false),
+  material_ptr(NULL),
+  hit_point(Vector3d::Zero()),
+  local_hit_point(Vector3d::Zero()),
+  normal(Vector3d::Zero()),
+  ray(),
+  depth(0),
+  t(0.0),
+  w(wr)
 {}
 
 
-// ------------------------------------------------------------------ copy constructor
-
-ShadeRec::ShadeRec(const ShadeRec& sr)
-	: 	hit_an_object(sr.hit_an_object),
-		material_ptr(sr.material_ptr),
-		hit_point(sr.hit_point),
-		local_hit_point(sr.local_hit_point),
-		normal(sr.normal),
-		ray(sr.ray),
-		depth(sr.depth),
-		t(sr.t),
-		w(sr.w)
+ShadeRec::ShadeRec(const ShadeRec& sr) :
+  hit_an_object(sr.hit_an_object),
+  material_ptr(sr.material_ptr),
+  hit_point(sr.hit_point),
+  local_hit_point(sr.local_hit_point),
+  normal(sr.normal),
+  ray(sr.ray),
+  depth(sr.depth),
+  t(sr.t),
+  w(sr.w)
 {}
 
 
-
-		
-		
-						
-		
+ShadeRec::~ShadeRec() {}
--- src/Utilities/ShadeRec.h
+++ src/Utilities/ShadeRec.h
@@ -1,38 +1,31 @@
 #ifndef __SHADE_REC__
 #define __SHADE_REC__
 
-// this file contains the declaration of the class ShadeRec
-
-#include <vector>
+#include "Constants.h"
+#include "Ray.h"
+#include "RGBColor.h"
+#include <Eigen/Dense>
+using Eigen::Vector3d;
 
 class Material;
 class World;
 
-// We need the following as #includes instead of forward class declarations,
-// because we have the objects themselves, not pointers or references
-
-#include "Point3D.h"
-#include "Normal.h"
-#include "Ray.h"
-#include "RGBColor.h"
-
 class ShadeRec {
-	public:
-	
-		bool				hit_an_object;		// Did the ray hit an object?
-		Material* 			material_ptr;		// Pointer to the nearest object's material
-		Point3D 			hit_point;			// World coordinates of intersection
-		Point3D				local_hit_point;	// World coordinates of hit point on generic object (used for texture transformations)
-		Normal				normal;				// Normal at hit point
-		Ray					ray;				// Required for specular highlights and area lights
-		int					depth;				// recursion depth
-		float				t;					// ray parameter
-		World&				w;					// World reference
-		RGBColor            color;
-				
-		ShadeRec(World& wr);					// constructor
-		
-		ShadeRec(const ShadeRec& sr);			// copy constructor
+  
+ public:
+  bool	    hit_an_object;      // did the ray hit an object?
+  Material* material_ptr;       // pointer to the nearest object's material
+  Vector3d  hit_point;          // world coordinates of intersection
+  Vector3d  local_hit_point;    // world coordinates of hit point on generic object (used for texture transformations)
+  Vector3d  normal;		// normal at hit point
+  Ray 	    ray;                // required for specular hightlights and area lights
+  int	    depth;              // recursion depth
+  float	    t;                  // ray parameter
+  World&    w;                  // world reference
+  ShadeRec(World& wr);
+  ShadeRec(const ShadeRec& sr);
+  ~ShadeRec();
+  
 };
 
 #endif
