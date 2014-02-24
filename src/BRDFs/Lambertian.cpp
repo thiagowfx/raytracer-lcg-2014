2b66dc5ad3aae13c127a89d7d3d83deb42bf8dbc
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-23 23:39:05 -0300

    lambertian ok

--- src/BRDFs/Lambertian.cpp
+++ src/BRDFs/Lambertian.cpp
@@ -1,66 +1,44 @@
 #include "Lambertian.h"
-#include "Constants.h"
 
-// ---------------------------------------------------------------------- default constructor
-
-Lambertian::Lambertian(void) 
-	:   BRDF(),
-		kd(0.0), 
-		cd(0.0)
+Lambertian::Lambertian() :
+  BRDF(),
+  kd(0.0), 
+  cd(0.0)
 {}
 
 
-// ---------------------------------------------------------------------- copy constructor
-
-Lambertian::Lambertian(const Lambertian& lamb) 
-	:   BRDF(lamb),
-		kd(lamb.kd), 
-		cd(lamb.cd)
+Lambertian::Lambertian(const Lambertian& lamb) :
+  BRDF(lamb),
+  kd(lamb.kd), 
+  cd(lamb.cd)
 {}
 
 
-// ---------------------------------------------------------------------- clone
-
-Lambertian* 
-Lambertian::clone(void) const {
-	return (new Lambertian(*this));
+Lambertian* Lambertian::clone() const {
+  return (new Lambertian(*this));
 }	
 
 
-// ---------------------------------------------------------------------- destructor
-
-Lambertian::~Lambertian(void) {}
+Lambertian::~Lambertian() {}
 
 
-// ---------------------------------------------------------------------- assignment operator
-
-Lambertian& 
-Lambertian::operator= (const Lambertian& rhs) {
-	if (this == &rhs)
-		return (*this);
-		
-	BRDF::operator= (rhs);
+Lambertian& Lambertian::operator= (const Lambertian& rhs) {
+  if (this != &rhs) {
+    BRDF::operator= (rhs);
 	
-	kd = rhs.kd; 
-	cd = rhs.cd;
+    kd = rhs.kd; 
+    cd = rhs.cd;
+  }
 	
-	return (*this);
+  return *this;
 }
 
 
-// ---------------------------------------------------------------------- f
-
-RGBColor
-Lambertian::f(const ShadeRec& sr, const Vector3D& wo, const Vector3D& wi) const {
-	return (kd * cd * invPI);
+RGBColor Lambertian::f(const ShadeRec& sr, const Vector3D& wo, const Vector3D& wi) const {
+  return kd * cd * invPI;
 }
 
 
-// ---------------------------------------------------------------------- rho
-
-RGBColor
-Lambertian::rho(const ShadeRec& sr, const Vector3D& wo) const {
-	return (kd * cd);
+RGBColor Lambertian::rho(const ShadeRec& sr, const Vector3D& wo) const {
+  return kd * cd;
 }
-
-
