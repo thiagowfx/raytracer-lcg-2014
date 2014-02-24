ce9bd1e4f71b0669c3cc86eec1aef45559b6e8b6
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-23 22:50:32 -0300

    pinhole OK

--- src/Cameras/Pinhole.cpp
+++ src/Cameras/Pinhole.cpp
@@ -1,103 +1,68 @@
-// This file contains the definition of the Pinhole class
-
-#include "Constants.h" 
-#include "Point3D.h"
-#include "Vector3D.h"
 #include "Pinhole.h"
-#include <math.h>
 
-// ----------------------------------------------------------------------------- default constructor
-
-Pinhole::Pinhole(void)		
-	:	Camera(),
-		d(500),
-		zoom(1.0)
+Pinhole::Pinhole() :
+  Camera(),
+  d(500),
+  zoom(1.0)
 {}
 
 
-// ----------------------------------------------------------------------------- copy constructor
-
-Pinhole::Pinhole(const Pinhole& c)   		
-	: 	Camera(c),
-		d(c.d),
-		zoom(c.zoom)
+Pinhole::Pinhole(const Pinhole& c) :
+  Camera(c),
+  d(c.d),
+  zoom(c.zoom)
 {}
 
 
-// ----------------------------------------------------------------------------- clone
+Pinhole& Pinhole::operator= (const Pinhole& rhs) {
+  if (this != &rhs) {
+    Camera::operator= (rhs);
+    d    = rhs.d;
+    zoom = rhs.zoom;
+  }
 
-Camera* 
-Pinhole::clone(void) const {
-	return (new Pinhole(*this));
+  return *this;
 }
 
 
-
-// ----------------------------------------------------------------------------- assignment operator
-
-Pinhole& 
-Pinhole::operator= (const Pinhole& rhs) { 	
-	if (this == &rhs)
-		return (*this);
-		
-	Camera::operator= (rhs);
-
-	d 		= rhs.d;
-	zoom	= rhs.zoom;
-
-	return (*this);
-}
+Pinhole::~Pinhole() {}
 
 
-// ----------------------------------------------------------------------------- destructor
-
-Pinhole::~Pinhole(void) {}	
-
-
-// ----------------------------------------------------------------------------- get_direction
-
-Vector3D
-Pinhole::get_direction(const Point2D& p) const {
-	Vector3D dir = p.x * u + p.y * v - d * w;
-	dir.normalize();
+Vector3d Pinhole::get_direction(const Vector2d& p) const {
+  Vector3d dir = p(0) * u + p(1) * v - d * w;
+  dir.normalize();
 	
-	return(dir);
+  return dir;
 }
 
 
-
-// ----------------------------------------------------------------------------- render_scene
-
-void 												
-Pinhole::render_scene(const World& w, FILE *fp) {
-	RGBColor	L;
-	ViewPlane	vp(w.vp);	 								
-	Ray			ray;
-	int 		depth = 0;  
-	Point2D 	pp;		// sample point on a pixel
-	int n = (int)sqrt((float)vp.num_samples);
+void Pinhole::render_scene(const World& w, FILE *fp) {
+  RGBColor  L;
+  ViewPlane vp(w.vp);
+  Ray	    ray;
+  int 	    depth = 0;  
+  Vector2d  pp;                 // sample point on a pixel
+  int       n     = (int)sqrt((float)vp.num_samples);
 		
-	vp.s /= zoom;
-	ray.o = eye;
+  vp.s /= zoom;
+  ray.o = eye;
 
-        fprintf(fp, "%d %d\n", vp.hres, vp.vres);
+  fprintf(fp, "%d %d\n", vp.hres, vp.vres);
 		
-	for (int r = 0; r < vp.vres; r++)			// up
-		for (int c = 0; c < vp.hres; c++) {		// across 					
-			L = black; 
+  for (int r = 0; r < vp.vres; r++) // up
+    for (int c = 0; c < vp.hres; c++) { // across 					
+      L = black; 
 			
-			for (int p = 0; p < n; p++)			// up pixel
-				for (int q = 0; q < n; q++) {	// across pixel
-					pp.x = vp.s * (c - 0.5 * vp.hres + (q + 0.5) / n); 
-					pp.y = vp.s * (r - 0.5 * vp.vres + (p + 0.5) / n);
-					ray.d = get_direction(pp);
-					L += w.tracer_ptr->trace_ray(ray, depth);
-				}	
+      for (int p = 0; p < n; p++)     // up pixel
+        for (int q = 0; q < n; q++) { // across pixel
+          pp(0) = vp.s * (c - 0.5 * vp.hres + (q + 0.5) / n); 
+          pp(1) = vp.s * (r - 0.5 * vp.vres + (p + 0.5) / n);
+          ray.d = get_direction(pp);
+          L += w.tracer_ptr->trace_ray(ray, depth);
+        }	
 											
-			L /= vp.num_samples;
-			L *= exposure_time;
-			w.display_pixel(r, c, L, fp);
-		} 
+      L /= vp.num_samples;
+      L *= exposure_time;
+      w.display_pixel(r, c, L, fp);
+    } 
 }
-
-
--- src/Cameras/Pinhole.h
+++ src/Cameras/Pinhole.h
@@ -1,65 +1,40 @@
 #ifndef __PINHOLE__
 #define __PINHOLE__
 
-// This file contains the declaration of the class Pinhole
-
-#include "Point2D.h"
+#include "Camera.h"
+#include "Constants.h"
 #include "World.h"    // we can #include "World.h" here
-
-//--------------------------------------------------------------------- class Pinhole
+#include <Eigen/Dense>
+#include <cmath>
+using Eigen::Vector2d;
+using Eigen::Vector3d;
 
 class Pinhole: public Camera {
-	public:
-	
-		Pinhole();   							
-
-		Pinhole(const Pinhole& ph);	
+  
+ public:
+  Pinhole();   							
+  Pinhole(const Pinhole& ph);	
+  Pinhole& operator= (const Pinhole& rhs);	
+  virtual ~Pinhole();   						
+  void set_view_distance(const float vpd);
+  void set_zoom(const float zoom_factor);
+  Vector3d get_direction(const Vector2d& p) const;
+  virtual void render_scene(const World& w, FILE *fp);
 		
-		virtual Camera*						
-		clone(void) const;			
-
-		Pinhole& 							
-		operator= (const Pinhole& rhs);	
-		
-		virtual
-		~Pinhole();   						
-			
-		void
-		set_view_distance(const float vpd);
-		
-		void
-		set_zoom(const float zoom_factor);
-		
-		Vector3D								
-		get_direction(const Point2D& p) const;
-		
-		virtual void 												
-                  render_scene(const World& w, FILE *fp);
-		
-	private:
-			
-		float	d;		// view plane distance
-		float	zoom;	// zoom factor
+ private:
+  float	d;                      // view plane distance
+  float	zoom;                   // zoom factor
 		
 };
 
 
-
-
-//-------------------------------------------------------------------------- set_vpd
-
-inline void
-Pinhole::set_view_distance(float _d) {
-	d = _d;
+inline void Pinhole::set_view_distance(float _d) {
+  d = _d;
 }	
-	
 
 
-//-------------------------------------------------------------------------- set_zoom
-
-inline void
-Pinhole::set_zoom(float zoom_factor) {
-	zoom = zoom_factor;
+inline void Pinhole::set_zoom(float zoom_factor) {
+  zoom = zoom_factor;
 }	
 
 #endif
