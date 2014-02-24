9e665b9982a61779ed74308ca96fe3798b5fbac7
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-24 18:58:05 -0300

    added Reflective

---
+++ src/Materials/Reflective.h
@@ -0,0 +1,49 @@
+#ifndef __REFLECTIVE__
+#define __REFLECTIVE__
+
+#include "Phong.h"
+#include "PerfectSpecular.h"
+
+class Reflective: public Phong {
+  
+ public:
+  Reflective();										
+  Reflective(const Reflective& rm);						
+  Reflective& 
+    operator= (const Reflective& rhs);						
+  virtual Reflective* clone() const;				
+  ~Reflective(); 
+  void set_kr(const float k);
+  void set_cr(const RGBColor& c);
+  void set_cr(const float r, const float g, const float b);
+  void set_cr(const float c);
+  virtual RGBColor shade(ShadeRec& s);
+		
+ private:
+  PerfectSpecular* reflective_brdf;
+  
+};
+
+
+inline void Reflective::set_kr(const float k) {
+  reflective_brdf->set_kr(k);
+}
+
+
+inline void Reflective::set_cr(const RGBColor& c) {
+  reflective_brdf->set_cr(c);
+	
+}
+
+
+inline void Reflective::set_cr(const float r, const float g, const float b) {
+  reflective_brdf->set_cr(r, g, b);
+}
+
+
+inline void Reflective::set_cr(const float c) {
+  reflective_brdf->set_cr(c);
+}
+
+
+#endif
