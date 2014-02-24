8bebe149c1216c5d45db1f3b6bc388528a6f9ed6
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-02-23 22:54:15 -0300

    GeometricObject OK

--- src/GeometricObjects/GeometricObject.cpp
+++ src/GeometricObjects/GeometricObject.cpp
@@ -1,62 +1,43 @@
-// this file contains the definition of the class GeometricObject 
-
-#include "Constants.h"
-#include "Material.h"
 #include "GeometricObject.h"
 
-
-// ---------------------------------------------------------------------- default constructor
-
-
-GeometricObject::GeometricObject(void)
-	: material_ptr(NULL)
+GeometricObject::GeometricObject() :
+  material_ptr(NULL)
 {}
 
 
-// ---------------------------------------------------------------------- copy constructor
-
 GeometricObject::GeometricObject (const GeometricObject& object) {
-	if(object.material_ptr)
-		material_ptr = object.material_ptr->clone(); 
-	else  material_ptr = NULL;
+  if (object.material_ptr)
+    material_ptr = object.material_ptr->clone();
+  else
+    material_ptr = NULL;
 }	
 
 
-// ---------------------------------------------------------------------- assignment operator
+GeometricObject& GeometricObject::operator= (const GeometricObject& rhs) {
+  
+  if (this != &rhs) {
+    
+    if (material_ptr) {
+      delete material_ptr;
+      material_ptr = NULL;
+    }
 
-GeometricObject&														
-GeometricObject::operator= (const GeometricObject& rhs) {
-	if (this == &rhs)
-		return (*this);
-			
-	if (material_ptr) {
-		delete material_ptr;
-		material_ptr = NULL;
-	}
+    if (rhs.material_ptr)
+      material_ptr = rhs.material_ptr->clone();
+  }
 
-	if (rhs.material_ptr)
-		material_ptr = rhs.material_ptr->clone();
-
-	return (*this);
+  return *this;
 }
 
 
-// ---------------------------------------------------------------------- destructor
-
-GeometricObject::~GeometricObject (void) {	
-	if (material_ptr) {
-		delete material_ptr;
-		material_ptr = NULL;
-	}
+GeometricObject::~GeometricObject () {
+  if (material_ptr) {
+    delete material_ptr;
+    material_ptr = NULL;
+  }
 }
 
 
-// ---------------------------------------------------------------- set_material
-
-void 
-GeometricObject::set_material(Material* mPtr) {
-	material_ptr = mPtr;
+void GeometricObject::set_material(Material* mPtr) {
+  material_ptr = mPtr;
 }
-
-
-								
--- src/GeometricObjects/GeometricObject.h
+++ src/GeometricObjects/GeometricObject.h
@@ -1,53 +1,34 @@
 #ifndef __GEOMETRIC_OBJECT__
 #define __GEOMETRIC_OBJECT__
 
-class Material;
-	
-#include "Point3D.h"
+#include "Constants.h"
 #include "Ray.h"
 #include "ShadeRec.h"
-
-
-//----------------------------------------------------------------------------------------------------- Class GeometricObject
+#include "Material.h"
+#include <Eigen/Dense>
+using Eigen::Vector3d;					
 
 class GeometricObject {	
 	
-	public:	
-
-		GeometricObject(void);									// default constructor
-		
-		GeometricObject(const GeometricObject& object);			// copy constructor
+ public:	
+  GeometricObject();		
+  GeometricObject(const GeometricObject& object); 
+  virtual GeometricObject* clone() const = 0;
+  virtual ~GeometricObject ();	
+  virtual bool hit(const Ray& ray, double& t, ShadeRec& s) const = 0;
+  Material* get_material() const;
+  virtual void set_material(Material* mPtr);
 	
-		virtual GeometricObject*								// virtual copy constructor
-		clone(void) const = 0;
-
-		virtual 												// destructor
-		~GeometricObject (void);	
-			
-		virtual bool 												 
-		hit(const Ray& ray, double& t, ShadeRec& s) const = 0;
-				
-		Material*						
-		get_material(void) const;
-
-		virtual void 							// needs to virtual so that it can be overriden in Compound
-		set_material(Material* mPtr); 			
-
-	
-	protected:
-	
-		mutable Material*   material_ptr;   	// mutable allows Compound::hit, Instance::hit and Grid::hit to assign to material_ptr. hit functions are const
-	
-		GeometricObject&						// assignment operator
-		operator= (const GeometricObject& rhs);
+ protected:
+  /* mutable allows Compound::hit, Instance::hit and Grid::hit to assign to material_ptr. hit functions are const */
+  mutable Material* material_ptr;
+  GeometricObject& operator= (const GeometricObject& rhs);
 };
 
 
-// ------------------------------------------------------------------------- get_material
-
-inline Material* 
-GeometricObject::get_material(void) const {
-	return (material_ptr);
+inline Material* GeometricObject::get_material() const {
+  return material_ptr;
 }
+
 
 #endif
