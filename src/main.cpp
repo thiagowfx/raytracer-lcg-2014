dd3c3e715470a679167590cb7f041590c1fa33c7
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-03-22 01:13:17 -0300

    preparing main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,43 @@
 #include "World.h"
 #include <cstdio>
 
-/* Builds the Ray Tracer */
-void build_raytracer();
+const char world_output[] = "world_output.txt";
+const char image_output[] = "image_output.png";
+
+void build_world();
 
 
 int main() {
-  build_raytracer();
+  build_world();
   
   return 0;
 }
 
 
-void build_raytracer() {
-  
+void build_world() {
   FILE *fp;
   World w;
 
-  /* World processing */
+  /* Set up and execute the Raytracer */
   if ( (fp = fopen("world_output.txt", "w")) != NULL) {
     w.build();
 
-    if (w.tracer_ptr == NULL) {
-      puts("ERROR: You forgot to set a world tracer");
-      exit(1);
-    }     
+    if (w.tracer_ptr == NULL)
+      runtime_error("ERROR: No world tracer set.");
    
     // w.render_scene(fp);
     w.camera_ptr->render_scene(w, fp);
     fclose(fp);
   }
-  else {
-    puts("ERROR: Can't open the file");
-    exit(1);
-  }
+  else
+    runtime_error("ERROR: Cannot open the file");
   
-  /* Image rendering */
-  if ( (fp = fopen("world_output.txt","r")) != NULL) {
-    w.file_to_png(fp, "output.png");  
+  /* Image Rendering */
+  if ( (fp = fopen(world_output,"r")) != NULL ) {
+    w.file_to_png(fp, image_output);
     fclose(fp);
   }
-  else {
-    puts("ERROR: Can't open the file");
-    exit(1);
-  }
+  else
+    runtime_error("ERROR: Cannot open the file");
+  
 }
