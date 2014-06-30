d48331d122a84e74257d392411cc4fef0002998d
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-06-30 18:48:33 -0300

    namespace Raytracer to remaining classes

--- lib/raytracer/GeometricObjects/PrimitivaDaniel.h
+++ lib/raytracer/GeometricObjects/PrimitivaDaniel.h
@@ -1,29 +1,25 @@
-#ifndef __PRIMITIVA_DANIEL__
-#define __PRIMITIVA_DANIEL__
+#ifndef _PRIMITIVA_DANIEL_MINE_
+#define _PRIMITIVA_DANIEL_MINE_
 
-/* RAYTRACER */
-#include <vector>
 #include "GeometricObject.h"
-
-/* PCS_DETECTION */
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include "pc-shape-detection.h"
-#include "primitive.h"
 
-class PrimitivaDaniel: public GeometricObject {
+namespace Raytracer {
+  class PrimitivaDaniel: public GeometricObject {
 
- public:
-  PrimitivaDaniel(Primitive*);
-  PrimitivaDaniel(const PrimitivaDaniel&);
-  ~PrimitivaDaniel();
-  virtual PrimitivaDaniel* clone() const;
-  virtual bool hit(const Ray_t& type, const Ray& ray, double& tmin, ShadeRec& sr) const;
-  static vector<PrimitivaDaniel*> dummy(const char* path_to_shape);
+  public:
+    PrimitivaDaniel(Primitive*);
+    PrimitivaDaniel(const PrimitivaDaniel&);
+    ~PrimitivaDaniel();
+    virtual PrimitivaDaniel* clone() const;
+    virtual bool hit(const Ray_t& type, const Ray& ray, double& tmin, ShadeRec& sr) const;
+    static vector<PrimitivaDaniel*> dummy(const char* path_to_shape);
 
- private:
-  Primitive* primitive;
-};
+  private:
+    Primitive* primitive;
+  };
+}
 
-
-#endif
+#endif // _PRIMITIVA_DANIEL_MINE_
