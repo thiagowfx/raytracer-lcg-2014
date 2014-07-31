55e31781d9f43427c704424b453a4aff23a00d3b
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-07-31 16:02:34 -0300

    removed portability errors

--- lib/fetch-pcs-detection.sh
+++ lib/fetch-pcs-detection.sh
@@ -16,11 +16,13 @@ error() {
     echo -e "$RED--> $*$RESET"
 }
 
-[[ -d "$WORKING_DIR" ]] || mkdir -p "$WORKING_DIR"
+if [ ! -d "$WORKING_DIR" ]; then
+    mkdir -p "$WORKING_DIR"
+fi
 cd $WORKING_DIR
 
 ########## AntTweakBar ##########
-if [[ ! -d AntTweakBar ]];
+if [ ! -d AntTweakBar ];
 then
     git clone git://git.code.sf.net/p/anttweakbar/code AntTweakBar
 fi
@@ -31,7 +33,7 @@ make && msg "AntTweakBar OK" || error "AntTweakBar failed"
 cd $WORKING_DIR
 
 ########## glew ##########
-if [[ ! -d glew ]]; then
+if [ ! -d glew ]; then
     git clone https://github.com/nigels-com/glew.git glew
 fi
 cd glew/
@@ -40,7 +42,7 @@ make extensions && make && msg "glew OK" || error "glew failed"
 cd $WORKING_DIR
 
 ########## vcglib ##########
-if [[ ! -d vcglib ]]; then
+if [ ! -d vcglib ]; then
     svn checkout svn://svn.code.sf.net/p/vcg/code/trunk/vcglib vcglib
 fi
 cd vcglib/
@@ -51,11 +53,11 @@ msg "vcglib OK"
 cd $WORKING_DIR
 
 ########## pcs-detection ##########
-if [[ ! -d pcs-detection ]]; then
+if [ ! -d pcs-detection ]; then
     git clone git://git.lcg.ufrj.br/daniel-coutinho/pcs-detection.git pcs-detection
-#    git clone git@git.lcg.ufrj.br:thiago-perrotta/pcs-detection.git pcs-detection
 fi
 cd pcs-detection/
 git pull origin master
-qmake && make && msg "pcs-detection OK" || error "pcs-detection failed"
+# qmake && make && msg "pcs-detection OK" || error "pcs-detection failed"
+msg "pcs-detection OK"
 cd $WORKING_DIR
